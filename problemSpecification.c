26d6af5110fd8a5607fa16c78a6937c107d4205e
halie-numinen <124086035+halie-numinen@users.noreply.github.com>
2026-03-03 20:30:38 -0500

    added to most of the print statements, I couldnt find one but I'll probably ask him about it tomorrow

--- problemSpecification.c
+++ problemSpecification.c
@@ -11,16 +11,20 @@ int main() {
     printf("Maximum number of semaphores per process (static): %d\n", _POSIX_SEM_NSEMS_MAX);
     printf("Maximum value of a counting semaphore (static): %d\n", _POSIX_SEM_VALUE_MAX);
     // I can't find the empircal ones
-    printf("Maximum value of a counting semaphore (empirical): \n");
-    printf("Maximum size of a shared memory segment (empirical): \n");
-    printf("Page size in bytes (dynamic): \n");
+    printf("Maximum value of a counting semaphore (empirical): %d\n", semncnt);
+    printf("Maximum size of a shared memory segment (empirical): %d\n", shmget);
+    int page = sysconf(PAGESIZE)
+    printf("Page size in bytes (dynamic): %d\n", page);
     long six = sysconf(_SC_PHYS_PAGES);
     printf("Physical pages in a system (dynamic): %ld\n", six);
     long seven = sysconf(_SC_CHILD_MAX);
     printf("Maximum number of processes per user (dynamic): %ld\n", seven);
-    printf("Maximum filesize in bytes (dynamic): \n");
-    printf("Maximum number of open files, hard limit (dynamic): \n");
+    int filesize = sysconf(FILESIZEBITS)
+    printf("Maximum filesize in bytes (dynamic): %d\n", filesize);
+    int open = sysconf(SC_OPEN_MAX)
+    printf("Maximum number of open files, hard limit (dynamic): %d\n", open);
     printf("Maximum number of open files, soft limit (dynamic): \n");
-    printf("Clock resolution in milliseconds (dynamic): \n");
+    int clock = clock_getres(clockid)
+    printf("Clock resolution in milliseconds (dynamic): %d\n", clock);
     return 0;
 }
