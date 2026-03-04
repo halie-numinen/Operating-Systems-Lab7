400ac79fb7d2ac5013892f722ae1ca18e0bd6019
Kahlan Walcott <166679300+Kahlan-walcott@users.noreply.github.com>
2026-03-04 14:04:53 -0500

    found another missing simicolen and fixed the second print statement variable.

--- problemSpecification.c
+++ problemSpecification.c
@@ -9,7 +9,7 @@
 // use the online man pages to find these (I copied the discription in the lab and searched the man pages)
 int main() {
     printf("Maximum number of semaphores per process (static): %d\n", _POSIX_SEM_NSEMS_MAX);
-    printf("Maximum value of a counting semaphore (static): %d\n", _POSIX_SEM_VALUE_MAX);
+    printf("Maximum value of a counting semaphore (static): %d\n", SEM_VALUE_MAX);
     // I can't find the empircal ones
     printf("Maximum value of a counting semaphore (empirical): %d\n", semncnt);
     printf("Maximum size of a shared memory segment (empirical): %d\n", shmget);
@@ -19,7 +19,7 @@ int main() {
     printf("Physical pages in a system (dynamic): %ld\n", six);
     long seven = sysconf(_SC_CHILD_MAX);
     printf("Maximum number of processes per user (dynamic): %ld\n", seven);
-    int filesize = sysconf(FILESIZEBITS)
+    int filesize = sysconf(FILESIZEBITS);
     printf("Maximum filesize in bytes (dynamic): %d\n", filesize);
     int open = sysconf(SC_OPEN_MAX);
     printf("Maximum number of open files, hard limit (dynamic): %d\n", open);
